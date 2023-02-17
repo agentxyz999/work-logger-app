f4accd68ef2f633d2ac8a11cadc73864098faeac
Rolie Conde <onefivezero999@gmail.com>
2023-02-17 18:24:41 +0800

    added some styling and delete icon

--- src/Projects.js
+++ src/Projects.js
@@ -1,63 +1,90 @@
-import React from 'react';
-import Card from 'react-bootstrap/Card';
-import './assets/Projects.css';
+import React from "react";
+import Card from "react-bootstrap/Card";
+import "./assets/Projects.css";
 
-function Projects({ personalProjects, workProjects }){
+function Projects({ personalProjects, workProjects }) {
+  let personalTotalTIme = 0;
+  let workTotalTIme = 0;
 
-    let personalTotalTIme = 0;
-    let workTotalTIme = 0;
+  personalProjects.forEach((item) => {
+    personalTotalTIme += parseInt(item.duration);
+  });
 
-    personalProjects.forEach((item) => {
-        personalTotalTIme += parseInt(item.duration);
+  workProjects.forEach((item) => {
+    workTotalTIme += parseInt(item.duration);
+  });
+
+  //get personal projects from local storage
+  const personalProjectsList = personalProjects
+    .sort((a, b) => b.duration - a.duration)
+    .map((personalProjects) => {
+      return (
+        <div className="personalProjContent" key={personalProjects.id}>
+          <p style={{ color: "green" }}>{personalProjects.duration}</p>
+          <p>{personalProjects.description}</p>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="red"
+            className="delete-btn"
+            viewBox="0 0 16 16"
+          >
+            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+          </svg>
+        </div>
+      );
     });
 
-    workProjects.forEach((item) => {
-        workTotalTIme += parseInt(item.duration);
+  //get work projects from local storage
+  const workProjectsList = workProjects
+    .sort((a, b) => b.duration - a.duration)
+    .map((workProject) => {
+      return (
+        <div className="workProjContent" key={workProject.id}>
+          <p style={{ color: "green" }}>{workProject.duration}</p>
+          <p>{workProject.description}</p>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="red"
+            className="delete-btn"
+            viewBox="0 0 16 16"
+          >
+            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+          </svg>
+        </div>
+      );
     });
 
-    //get personal projects from local storage
-    const personalProjectsList = personalProjects 
-        .sort((a, b) => b.duration - a.duration)
-        .map((personalProjects) => {
-        return(       
-            <p style={{color: 'green'}}>{ personalProjects.duration } <span> - { personalProjects.description }</span></p>   
-        );  
-    });
+  return (
+    <div className="container mt-1">
+      <h2 className="text-center">Projects</h2>
+      <div className="all-projects-container">
+        <Card className="personalProjects-container" key={personalProjects.id}>
+          <Card.Header className="card-header">
+            <h3 className="text-left"> Personal </h3>
+            <p style={{ color: "#0D6BF7", fontSize: "20px" }}>
+              {personalTotalTIme} minutes
+            </p>
+          </Card.Header>
+          <Card.Body>{personalProjectsList}</Card.Body>
+        </Card>
 
-    //get work projects from local storage
-    const workProjectsList = workProjects
-        .sort((a, b) => b.duration - a.duration)
-        .map((workProject) => {
-        return(
-            <p style={{color: 'green'}}>{ workProject.duration }<span> - { workProject.description }</span></p>
-        );  
-    });
-
-    return (
-            <div className='container mt-1'>
-                <h2 className='text-center'>Projects</h2>
-                <div className='all-projects-container'>
-                    <Card className='personalProjects-container' key={personalProjects.id}>
-                        <Card.Header className='card-header'>
-                            <h3 className='text-left'> Personal </h3>
-                            <p style={{color: '#0D6BF7', fontSize: '20px'}}> {personalTotalTIme} </p>
-                        </Card.Header>
-                        <Card.Body>
-                            {personalProjectsList}
-                        </Card.Body>
-                    </Card>
-                    
-                    <Card className='workProject-container' key={workProjects.id}>
-                        <Card.Header className='card-header'>
-                                <h3> Work </h3>
-                                <p style={{color: '#0D6BF7', fontSize: '20px'}}> {workTotalTIme} </p>
-                        </Card.Header>
-                        <Card.Body>
-                            {workProjectsList}
-                        </Card.Body>
-                    </Card>
-                </div>   
-            </div>    
-    );
+        <Card className="workProject-container" key={workProjects.id}>
+          <Card.Header className="card-header">
+            <h3> Work </h3>
+            <p style={{ color: "#0D6BF7", fontSize: "20px" }}>
+              {workTotalTIme} minutes
+            </p>
+          </Card.Header>
+          <Card.Body>{workProjectsList}</Card.Body>
+        </Card>
+      </div>
+    </div>
+  );
 }
 export default Projects;
